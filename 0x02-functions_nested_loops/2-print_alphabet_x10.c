4dc4caf9e7cc334b29abfdd0ced9128dc93f9ea1
getacupoftea <tezebada@gmail.com>
2023-03-22 13:45:55 -0700

    made changed to my alpahabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,11 +5,12 @@
  *  * print_alphabet_x10 - Make alphabet x10 times
  *   *
  *    * Return: void
- *     */
+ */
 
 void print_alphabet_x10(void)
 {
 	int i, j;
+
 	i = 97;
 	j = 1;
 	while (j <= 10)
