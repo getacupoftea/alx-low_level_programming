2425fc7e1663f440c5fda1d5bbd80be63e7d43ef
getacupoftea <tezebada@gmail.com>
2023-03-23 15:54:39 -0700

    diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_square - Entry point
+ * Description: checks is a character
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+
+void print_square(int c)
+{
+	int size, i, j;
+
+	size = c;
+
+	if (!(c <= 0))
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+			_putchar('#');
+			}
+			if (i < size)
+			{
+			_putchar('\n');
+			}
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
