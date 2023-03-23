54ce32690acac25303d0d71919802e283dd1befb
getacupoftea <tezebada@gmail.com>
2023-03-23 15:51:08 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_triangle - Entry point
+ * Description: checks is a character is uppercase
+ * @c: the integer value it receives
+ * Return: 1 if true. 0 if false.
+ */
+
+void print_triangle(int c)
+{
+	int size, i, j, k;
+
+	size = c;
+
+	if (!(c <= 0))
+	{
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i ; j++)
+			{
+				_putchar(' ');
+			}
+			for (k = 1; k <= i ; k++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
