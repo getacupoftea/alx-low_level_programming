4a369a931f0474a19b98eb3dfdd1c73c48f78e33
getacupoftea <tezebada@gmail.com>
2023-03-27 12:09:36 -0700

    parameter

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,2 +1,28 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * puts2 - print  string
+ * @str: pointer parameter a
+ * Description: print strings  value
+ */
+
+void puts2(char *str)
+{
+	int i, k;
+	int h;
+i = 0;
+	while (str[i])
+	{
+		i++;
+	}
+k = 1;
+	for (h = 0; h < i; h++)
+	{
+		if (h % 2 == 0)
+		{
+			printf("%c", str[h]);
+		}
+k++;
+	}
+	printf("\n");
+}
