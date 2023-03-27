f53888708705f81d799c9fd47614dc334b8d1a7e
Temitope Ezebada <125554239+getacupoftea@users.noreply.github.com>
2023-03-27 14:44:28 +0100

    Update 1-swap2.c

--- 0x05-pointers_arrays_strings/1-swap2.c
+++ 0x05-pointers_arrays_strings/1-swap2.c
@@ -1,2 +1,16 @@
 #include "main.h"
-#include <stdio.h>
+/**
+ * swap_int - swap the a value to b
+ * @a: pointer parameter a
+ * @b: variable b
+ * Description: change pointer value
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+
+int swap = *a;
+*a = *b;
+*b = swap;
+}
