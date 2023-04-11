ace12569bc1ceb98574d64ab2a21abf2472333b7
getacupoftea <tezebada@gmail.com>
2023-04-11 15:14:43 -0700

    betty

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,33 +3,43 @@
 		
 /**
 		
- * create_array - create array of size size and assign char c
+* create_array - create array of size size and assign char c
 		
- * @size: size of array
+* @size: size of array
 		
- * @c: char to assign
+* @c: char to assign
 		
- * Description: creat array of size size and assign char c
+* Description: creat array of size size and assign char c
 		
- * Return: pointer to array, NULL if fail
+* Return: pointer to array, NULL if fail
 		
- */
+*
+		
+*/
 		
 char *create_array(unsigned int size, char c)
 		
 {
 		
-char *str;
-unsigned int i;
+	char *str;
 		
-str = malloc(sizeof(char) * size);
-if (size == 0 || str == NULL)
+	unsigned int i;
 		
-return (NULL);
-		
-for (i = 0; i < size; i++)
-str[i] = c;
 
-return (str);
+		
+	str = malloc(sizeof(char) * size);
+		
+	if (size == 0 || str == NULL)
+		
+		return (NULL);
+		
+
+		
+	for (i = 0; i < size; i++)
+		
+		str[i] = c;
+		
+	return (str);
 		
 }
+
