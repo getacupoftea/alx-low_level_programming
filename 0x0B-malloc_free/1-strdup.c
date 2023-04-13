e241c46e9b0087e1bf213fcc912818477f1d8aad
getacupoftea <tezebada@gmail.com>
2023-04-12 18:55:12 -0700

    betty ish

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
-		
+
 #include <stdlib.h>
-		
+
 #include "main.h"
-		
+
 /**
-		
+
  * _strdup - duplicate to new memory space location
-		
+
  * @str: char
-		
+
  * Return: 0
-		
+
  */
-		
+
 char *_strdup(char *str)
-		
+
 {
-		
+
 	char *aaa;
 		
 	int i, r = 0;
