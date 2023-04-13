694a290be2d40fef45d13f6a2c343f51f585190f
getacupoftea <tezebada@gmail.com>
2023-04-13 15:20:25 -0700

    update

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,13 +1,12 @@
 #include <stdlib.h>
 #include "main.h"
 
-
-
 /**
  * _memset -fills memory with a constant byte
  * @s: memory area to be filled
  * @b: char to copy
  * @n :number of times to copy b
+ * Return: 0
  */
 
 char *_memset(char *s, char b, unsigned int n)
