ec14f59800657ec9a79b15d36e107efdf009f1ef
getacupoftea <tezebada@gmail.com>
2023-04-13 17:19:23 -0700

    update

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,17 +1,11 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include "main.h"
 
 /**
-
  * _strdup - duplicate to new memory space location
-
  * @str: char
-
  * Return: 0
-
  */
 
 char *_strdup(char *str)
