ce85ac7a154cc9d9d63fdccebcc86265893e27d7
getacupoftea <tezebada@gmail.com>
2023-04-17 12:19:07 -0700

    main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - print name of file 
+ * main - print name of file
  *
  * Return: Always 0.
  */
 int main(void)
 {
 
-	printf("File :%s\n", __FILE__);
-	return 0;
+	printf("%s\n", __FILE__);
+	return (0);
 
 }
