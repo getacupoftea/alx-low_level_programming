ae4fe79aae51b85c283853964d514b1c1575388d
getacupoftea <tezebada@gmail.com>
2023-12-15 01:57:58 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - a C progam that prints,
+ * with proper grammar, but the outcome is a piece of art,
+ * followed by a new line
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
