227e726420b18c1a377f9a2380fd412cb1e388fb
getacupoftea <tezebada@gmail.com>
2023-12-16 01:06:20 -0800

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,6 +5,7 @@
  * with proper grammar, but the outcome is a piece of art,
  * followed by a new line
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
