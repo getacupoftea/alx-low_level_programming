ec75f8dc4f10de47ce77070cc57a6d3758acd9de
getacupoftea <tezebada@gmail.com>
2024-01-02 16:05:41 -0800

    reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Update the value that the pointer n is pointing to
+ * @n - is the pointer
+ * Return: 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef MY_HEADER_FILE_H
+#define MY_HEADER_FILE_H
+
+void reset_to_98(int *n);
+
+#endif
+
